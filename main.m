127c04d13ad6beae77874ccad3244367566fc1af
Lnafleury <efleury1206@gmail.com>
2025-05-29 13:08:27 +0200

    ajout diag de l'oeil, constellation, TEB
    ajout diagramme de l'oeil, constellation, TEB

--- main.m
+++ main.m
@@ -2,11 +2,26 @@ clear all;
 close all;
 addpath('signal');
 
-bits = [0 1 1 0 0 1];
-Fe = 24000;
-Rb = 3000;
-[signal_bpsk, signal_recu_filtre_bpsk, echelle, echelle_filtre] = modulation_bpsk_multitrajet(bits, Fe, Rb);
+%paramètres
+bits = [0 1 1 0 0 1]; %bits
+Fe = 24000;           %Fréquence d'échantillonage
+Rb = 3000;            %Débit binaire
+[signal_bpsk, signal_recu_filtre_bpsk, echelle, echelle_filtre, oeil_bpsk, TEB_bpsk] = modulation_bpsk_multitrajet(bits, Fe, Rb);
 
+%tracé du signal reçu en sortie du filtre de réception bpsk
 figure
 plot(echelle_filtre, signal_recu_filtre_bpsk, 'o');
 title('Signal BPSK modulé');
+
+%tracé du diagramme de l'oeil en sortie du filtre de réception bpsk
+figure
+plot(oeil_bpsk);
+title(['Tracé du du diagramme de l oeil en sortie du filtre de réception bpsk']);
+
+%tracé de la constellation en sortie du filtre de réception bpsk
+figure
+plot(real(signal_bpsk), imag(signal_bpsk), 'o');
+xlabel('a_k');
+ylabel('b_k');
+title(['Tracé de la constellation en sortie du filtre de réception bpsk']);
+
